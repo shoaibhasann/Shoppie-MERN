292ebe0f6f18467a48eeec9754dbdab114dc4ea0
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-13 23:37:04 +0530

    add uncaught exceptions handler

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ import connectDatabase from "./config/db.js";
 // loding enviroment variables
 dotenv.config();
 
+// Handling uncaught exceptions
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+});
+
+
 const port = process.env.PORT || 8000;
 
 // start the server
