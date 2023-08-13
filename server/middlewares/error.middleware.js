12148a14c69252817b415577867040ef61caf2e2
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-13 23:36:34 +0530

    enhance error middleware

--- server/middlewares/error.middleware.js
+++ server/middlewares/error.middleware.js
@@ -1,6 +1,17 @@
+import AppError from "../utils/error.util.js";
+
 const errorMiddleware = (err,req,res,next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Something went wrong';
+
+    // Handling MongoDB error
+    if(err.name === 'CastError'){
+        const message = `Resource not found, Invalid: ${err.path}`
+        return res.status(400).json({
+            success: false,
+            message,
+        });
+    }
 
     return res.status(err.statusCode).json({
         success: false,
