58f71dfabbf8ad08c262b6ba21c6c37b93ba7e39
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-13 22:46:47 +0530

    create error utility function for error handling

---
+++ server/utils/error.util.js
@@ -0,0 +1,12 @@
+class AppError extends Error{
+    constructor(statusCode, message){
+        super(message);
+
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    };
+
+}
+
+export default AppError;
