d83de0e38fbd4467a9134afb042f7c9c6df926a6
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-15 03:35:30 +0530

    create new user routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
-import { loginUser, registerUser } from '../controllers/user.controller.js';
-
+import { forgotPassword, loginUser, logoutUser, registerUser, resetPassword } from '../controllers/user.controller.js';
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/password/forgot', forgotPassword);
+router.put('/password/reset/:resetToken', resetPassword);
+router.get('/logout', isLoggedIn, logoutUser);
+
 
 export default router;
