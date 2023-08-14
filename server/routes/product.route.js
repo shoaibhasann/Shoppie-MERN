737e13a944268948654f5bb1cab96a9b1e9de4af
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-15 03:35:09 +0530

    add authentication and authorization to product routes

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createProduct, deleteProduct, getAllProducts, productDetails, updateProduct } from "../controllers/product.controller.js";
+import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post('/', createProduct);
-router.get('/', getAllProducts);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+router.post('/', isLoggedIn, authorizedRoles('Admin'), createProduct);
+router.get("/", getAllProducts);
+router.put("/:id", isLoggedIn, authorizedRoles("Admin"), updateProduct);
+router.delete("/:id", isLoggedIn, authorizedRoles("Admin"), deleteProduct);
 router.get('/:id', productDetails);
 
 export default router;
