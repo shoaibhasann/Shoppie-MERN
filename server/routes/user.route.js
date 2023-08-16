370cf13ee29e362ff6774ccfaaf8cc85bef05451
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-16 15:30:30 +0530

    refactor user routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,32 +1,48 @@
-import { Router } from 'express'
-import { changePassword, deleteUser, forgotPassword, getAllUsers, getProfile, getUserDetails, loginUser, logoutUser, registerUser, resetPassword, updateProfile, updateRole } from '../controllers/user.controller.js';
+import { Router } from "express";
+import {
+  changePassword,
+  deleteUser,
+  forgotPassword,
+  getAllUsers,
+  getProfile,
+  getUserDetails,
+  loginUser,
+  logoutUser,
+  registerUser,
+  resetPassword,
+  updateProfile,
+  updateRole,
+} from "../controllers/user.controller.js";
 import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post('/register', registerUser);
+// Public Routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:resetToken", resetPassword);
 
-router.post('/login', loginUser);
+// Private Routes (require authentication)
+router.put("/password/update", isLoggedIn, changePassword);
+router.put("/me/update", isLoggedIn, updateProfile);
+router.get("/logout", isLoggedIn, logoutUser);
+router.get("/me", isLoggedIn, getProfile);
 
-router.post('/password/forgot', forgotPassword);
-
-router.put('/password/reset/:resetToken', resetPassword);
-
-router.put('/password/update', isLoggedIn, changePassword);
-
-router.put('/me/update', isLoggedIn, updateProfile);
-
-router.put('/admin/user/:id', isLoggedIn, authorizedRoles('Admin'), updateRole);
-
-router.get('/logout', isLoggedIn, logoutUser);
-
-router.get('/me', isLoggedIn, getProfile);
-
+// Admin Routes (require authentication and admin role)
+router.put("/admin/user/:id", isLoggedIn, authorizedRoles("Admin"), updateRole);
 router.get("/admin/users", isLoggedIn, authorizedRoles("Admin"), getAllUsers);
-
-router.get("/admin/user/:id", isLoggedIn, authorizedRoles("Admin"), getUserDetails);
-
-router.delete('/admin/user/:id', isLoggedIn, authorizedRoles('Admin'), deleteUser);
-
+router.get(
+  "/admin/user/:id",
+  isLoggedIn,
+  authorizedRoles("Admin"),
+  getUserDetails
+);
+router.delete(
+  "/admin/user/:id",
+  isLoggedIn,
+  authorizedRoles("Admin"),
+  deleteUser
+);
 
 export default router;
