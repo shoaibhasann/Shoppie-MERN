ce37b0e39cbe073dcbbff2df74fdb4a7f877ce22
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-16 14:26:49 +0530

    add new routes in product model

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
-import { createProduct, deleteProduct, getAllProducts, productDetails, updateProduct } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts, productDetails, productReview, updateProduct } from "../controllers/product.controller.js";
 import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
+
 
 const router = Router();
 
-router.post('/', isLoggedIn, authorizedRoles('Admin'), createProduct);
+router.post('/admin', isLoggedIn, authorizedRoles('Admin'), createProduct);
+
 router.get("/", getAllProducts);
-router.put("/:id", isLoggedIn, authorizedRoles("Admin"), updateProduct);
-router.delete("/:id", isLoggedIn, authorizedRoles("Admin"), deleteProduct);
+
+router.put("/admin/:id", isLoggedIn, authorizedRoles("Admin"), updateProduct);
+
+router.put('/review', isLoggedIn, productReview);
+
+router.delete("/admin/:id", isLoggedIn, authorizedRoles("Admin"), deleteProduct);
+
 router.get('/:id', productDetails);
 
 export default router;
