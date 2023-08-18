de99586bbac4a368a3cc40893b1b5a2cd8659d17
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-18 16:48:06 +0530

    create redux store

--- client/src/features/Store.jsx
+++ client/src/features/Store.jsx
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from './productSlice.jsx';
+import productDetailReducer from "./productDetailSlice.jsx";
 
 
 const store = configureStore({
   reducer: {
-    products: productReducer
+
+    products: productReducer,
+
+    productDetail: productDetailReducer,
+    
   }
 });
 
