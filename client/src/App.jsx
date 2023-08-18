e3f9fa8db26c5309cbef6fa0ffc5980083e4ba6c
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-18 16:50:51 +0530

    day 2 completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/ProductDetail";
 
 
 function App() {
 
   return (
+
+    
     <Router>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/product/:id" element={<Product />} />
       </Routes>
     </Router>
   );
