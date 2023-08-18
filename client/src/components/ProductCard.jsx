bfe110f13e57f507e2acef535f27ea1b75dc2a30
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-18 16:47:33 +0530

    create product card and fetch product data from server

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,30 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import ReactStars from 'react-rating-stars-component';
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { numberWithCommas } from '../utils/utility';
 
-const options = {
-    edit: false,
-    color: "rgba(20,20,20,0.1)",
-    activeColor: "tomato",
-    size: 20,
-    value: 2.5,
-    isHalf: true
-}
 
 function ProductCard({ product }) {
+
+  const options = {
+    edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "#faca15",
+    size: 20,
+    value: product.ratings,
+    isHalf: true,
+  };
+
   return (
-    <Link>
-      <div className="flex flex-col gap-3 my-6 w-full max-w-[280px] hover:shadow-xl border border-slate-950">
-        <img src={product.url} alt={product.name} />
+    <Link to={`/product/${product._id}`}>
+      <div className="flex flex-col gap-3 my-6 w-[280px] hover:shadow-xl border border-slate-950">
+        <img src={product.images[0]} alt={product.name} />
         <p className="text-2xl font-bold mx-4">{product.name}</p>
         <div className="flex items-center gap-5 mx-4">
-          <ReactStars {...options} /> <span>256 reviews</span>
+          <ReactStars {...options} /> <span>{product.numberOfReviews}</span>
         </div>
-        <div className='flex items-center gap-5 mx-4 mb-4'>
+        <div className="flex items-center gap-5 mx-4 mb-4">
           <span className="text-xl font-semibold">
-            {product.price}
+            {numberWithCommas(product.price)}
           </span>
-          <button className='text-2xl px-4 py-2 text-[#222222]'><AiOutlineShoppingCart/></button>
+          <button className="text-2xl px-4 py-2 text-[#222222]">
+            <AiOutlineShoppingCart />
+          </button>
         </div>
       </div>
     </Link>
