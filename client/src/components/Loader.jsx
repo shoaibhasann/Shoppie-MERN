592d279cb380eb477fe4f5a0c1fcc4ac819a20b0
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-18 16:46:41 +0530

    create loader component

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Spinner } from 'flowbite-react';
+
+function Loader() {
+  return <Spinner aria-label="Pink spinner example" color="pink" size="xl" />;
+}
+
+export default Loader
