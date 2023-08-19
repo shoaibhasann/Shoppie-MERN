7182a726b0b5c408abff83b8a81c1b15b140f141
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-19 23:42:54 +0530

    changed App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Product from "./pages/ProductDetail";
 import ProductDetail from "./pages/ProductDetail";
 
 
 function App() {
 
   return (
-
-    
     <Router>
       <Navbar />
       <Routes>
