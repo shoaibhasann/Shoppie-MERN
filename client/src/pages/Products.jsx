8294c560fd46e48f7202d05f8c27b8a1d9d6f852
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-22 10:46:56 +0530

    make api request for search functionality

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -5,13 +5,21 @@ import MetaData from "../components/MetaData";
 import { STATUSES, fetchProducts } from "../features/productSlice";
 import ErrorHandler from "../components/ErrorHandler";
 import Loader from "../components/Loader";
+import { useParams } from "react-router-dom";
 
 function Products() {
   const dispatch = useDispatch();
 
+  const {keyword} = useParams();
+  console.log(keyword);
+
   useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
+    if(keyword){
+      dispatch(fetchProducts(keyword));
+    } else{
+      dispatch(fetchProducts());
+    }
+  }, [dispatch, keyword]);
 
   const {
     data: { products, productsCount },
@@ -26,8 +34,8 @@ function Products() {
   return (
     <>
       <MetaData title="All Products" />
-      <h1 className="text-3xl text-center border-b border-[#222222] w-[300px] mx-auto my-10">
-        All Products
+      <h1 className="text-3xl text-center mx-auto my-10">
+        {keyword ? `Products related to: ${keyword}` : 'All Products'}
       </h1>
       {status === STATUSES.SUCCESS ? (
         <div className="flex justify-center flex-wrap gap-5">
