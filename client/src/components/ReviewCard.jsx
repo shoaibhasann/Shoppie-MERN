5bc10079ce35b77ebf45af205e10b83d78838fd2
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-22 07:57:05 +0530

    complete review card

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
+import React from "react";
 import ReactStars from "react-rating-stars-component";
 
-function ReviewCard() {
+function ReviewCard({ review }) {
+  
+  const formatDate = (isoDate) => {
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    return new Date(isoDate).toLocaleDateString(undefined, options);
+  };
 
-         const options = {
-           edit: false,
-           color: "rgba(20,20,20,0.1)",
-           activeColor: "#faca15",
-           size: window.innerWidth < 600 ? 20 : 25,
-           value: 4.3,
-           isHalf: true,
-         };
+  const options = {
+    edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "#faca15",
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: review.rating,
+    isHalf: true,
+  };
   return (
     <div className="max-w-sm p-6 flex flex-col gap-5 bg-white border border-gray-200 rounded-lg shadow">
       <div className="flex items-center justify-between">
         <div className="flex gap-2">
           <div className="w-7 h-7 flex items-center justify-center text-white rounded-full bg-[#ed0010]">
-            S
+            {review.name.slice(0, 1)}
           </div>
-          <span className="text-lg font-bold">Shoaib Hasan</span>
+          <span className="text-xl font-bold">{review.name}</span>
         </div>
         <ReactStars {...options} />
       </div>
-      <p>
-        The device has a clean design and the metal housing feels sturdy in my
-        hands. Soft rounded corners make it a pleasure to look at.
-      </p>
-      <span>Feb 13, 2021</span>
+      <p className="text-lg font-normal">{review.comment}</p>
+      <span>{formatDate(review.createdAt)}</span>
     </div>
   );
 }
 
-export default ReviewCard
+export default ReviewCard;
