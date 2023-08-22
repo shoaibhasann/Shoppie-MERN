693f43f47afa4b112931f4fe3c61425ba0964f82
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-22 07:56:47 +0530

    complete reviews component

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,34 +1,26 @@
-import React from 'react'
-import ReviewCard from './ReviewCard';
+import React from "react";
+import ReviewCard from "./ReviewCard";
+import { useSelector } from "react-redux";
 
 function Reviews() {
-
-     const reviews = [1,2,3];
+  const product = useSelector((state) => state.productDetail.data.product);
 
   return (
     <div className="my-10 mx-auto">
       <h1 className="text-3xl text-center border-b border-[#222222] w-[300px] mx-auto my-20">
         Reviews
       </h1>
-      <div className='max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5 place-items-center'>
-        {reviews.length > 0 ? (
-          <>
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-          </>
-        ) : (
-          <div className="text-2xl text-center">No Reviews Yet</div>
-        )}
-      </div>
+      {product.reviews.length > 0 ? (
+        <div className="max-w-7xl px-5 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-5 place-items-center place-content-center">
+            {product.reviews.map((review, index) => (
+            <ReviewCard key={index} review={review} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="text-center text-2xl font-medium">No Reviews Yet</h2>
+      )}
     </div>
   );
 }
 
-export default Reviews
+export default Reviews;
