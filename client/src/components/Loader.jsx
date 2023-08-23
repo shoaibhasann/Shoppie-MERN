d828d4c568196c5e75700e032599c82fde12f825
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-23 20:27:15 +0530

    change styling

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Loader() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#ed0010] border-opacity-80"></div>
+    <div className="fixed inset-0 flex justify-center items-center z-50">
+      <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-[#ed0010] border-opacity-80"></div>
     </div>
   );
 }
 
 export default Loader;
-
-
-
-
-
-
