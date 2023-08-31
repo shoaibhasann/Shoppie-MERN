0d70f76e2d3253fdc8f6f59d1b3ce8aff559b5a4
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-31 19:13:06 +0530

    create function for protected routes to authenticate user

---
+++ client/src/utils/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ element: Component, isAuthenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      element={isAuthenticated ? <Component /> : <Navigate to="/login" />}
+    />
+  );
+};
+
+export default ProtectedRoute;
