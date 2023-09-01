c1fd734dc2327db27cf701745886a06a61c5e602
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-01 10:00:11 +0530

    add more actions in slice

--- client/src/redux/ForgotPasswordSlice.jsx
+++ client/src/redux/ForgotPasswordSlice.jsx
@@ -1,18 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const forgotSlice = createSlice({
     name: 'forgotPassword',
     initialState: {
-        password: null,
         loading: false,
         error: null,
         message: null
     },
     reducers: {
-        setPassword(state,action){
-            state.password = action.payload;
-            state.error = null;
-        },
         setLoading(state,action){
             state.loading = action.payload;
         },
@@ -22,13 +18,38 @@ const forgotSlice = createSlice({
         setMessage(state,action){
             state.message = action.payload;
         },
+        clearMessage(state,action){
+            state.message = null;
+        },
         clearErrors(state,action){
             state.error = null;
         }
     }
 });
 
-export const { setPassword, setLoading, setError, setMessage, clearErrors } = forgotSlice.actions;
+export function forgotPassword(email){
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    
+    const server = "http://localhost:8080/api/v1/password/forgot";
+
+    return async function forgotPasswordThunk(dispatch, getState){
+        dispatch(setLoading(true));
+        try {
+            const { data } = await axios.post(server, email, config);
+            dispatch(setLoading(false));
+            dispatch(setMessage(data.message));
+        } catch (error) {
+            dispatch(setLoading(false));
+            dispatch(setError(error.response.data));
+            dispatch(clearMessage());
+        }
+    }
+}
+
+export const { setPassword, setLoading, setError, setMessage, clearErrors, clearMessage } = forgotSlice.actions;
 
 
 export default forgotSlice.reducer;
