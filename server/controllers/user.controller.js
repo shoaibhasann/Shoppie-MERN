74b74839067bd5c6bc6f48c2a8b50adadbb58ff9
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-01 10:00:37 +0530

    styles email message template

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -298,14 +298,29 @@ const forgotPassword = async (req, res, next) => {
     const subject = "Shoppie Password Recovery";
 
     // Create the content of the reset password email as an HTML link
-    const message = `
-      <h1>Dear ${emailExists.name},</h1>
-      <p>You have requested to reset your password. Please click on the following link to reset your password:</p>
-      <a href="${forgotPasswordURL}">${forgotPasswordURL}</a>
-      <p>If you did not request this password reset, you can ignore this email.</p>
-      <p>Best regards,</p>
-      <p>Shoppie</p>
-    `;
+const emailStyles = `
+  <div style="background-color: #f5f5f5; padding: 20px; font-family: Arial, sans-serif;">
+    <div style="background-color: #ed0010; text-align: center; padding: 10px;">
+      <h1 style="font-size: 30px; font-weight:700;">Shoppie</h1>
+    </div>
+    <div style="background-color: white; padding: 20px; border-radius: 5px; box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);">
+      <h1 style="color: #333; font-size: 24px; margin-bottom: 10px;">Dear ${emailExists.name},</h1>
+      <p style="color: #333;">You have requested to reset your password. Please click on the following link to reset your password:</p>
+      <a href="${forgotPasswordURL}" style="color: #ed0010; text-decoration: none;">${forgotPasswordURL}</a>
+      <p style="color: #333;">If you did not request this password reset, you can ignore this email.</p>
+      <p style="color: #333;">Best regards,</p>
+      <p style="color: #333;">Shoppie</p>
+    </div>
+  </div>
+`;
+
+const message = `
+  <html>
+    <body>
+      ${emailStyles}
+    </body>
+  </html>
+`;
 
     try {
       // send reset password email to the user
