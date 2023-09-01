5929684d55e0d4075627b6cafbe617a4b6cb3a19
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-01 08:21:58 +0530

    create function to handle error boundary

---
+++ client/src/utils/ErrorBoundary.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ hasError: true });
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
