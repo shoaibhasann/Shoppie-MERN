0292c9cbf1f662dd4b47c1513e3f4e5654d997fc
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-01 10:23:15 +0530

    add status code in error handler component

--- client/src/components/ErrorHandler.jsx
+++ client/src/components/ErrorHandler.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function ErrorHandler({message}) {
+function ErrorHandler({message, status}) {
   return (
     <section className="bg-white dark:bg-gray-900 ">
       <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
         <div className="wf-ull lg:w-1/2">
           <p className="text-xl font-medium text-[#e50010]">
-            OOPS! 404 ERROR
+            {`OOPS! ${status || 404} ERROR`}
           </p>
           <h1 className="mt-3 text-3xl font-semibold text-gray-800 dark:text-white md:text-3xl">
             {message}
@@ -14,7 +14,6 @@ function ErrorHandler({message}) {
           <p className=" text-lg mt-4 text-gray-500 dark:text-gray-400">
             Sorry, the page you are looking for doesn't exist or may be lost.
           </p>
-
         </div>
 
         <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
