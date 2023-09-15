fb715e749c6508d6ae873241c1ea1715b0427617
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-15 21:57:47 +0530

    create checkoutsteps component

---
+++ client/src/components/cart/CheckOutSteps.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function CheckOutSteps({ activeStep, setActiveStep }) {
+  const steps = [
+    { name: "Shipping", icon: "🚚" },
+    { name: "Confirm", icon: "📦" },
+    { name: "Payment", icon: "💳" },
+  ];
+
+  return (
+    <div className=" cursor-pointer max-w-md mx-4 lg:mx-auto flex justify-between items-center my-6 lg:my-10">
+      {steps.map((stepItem, index) => (
+        <div
+          key={index}
+          className={`flex items-center ${
+            index === activeStep ? "text-red-500" : "text-gray-500"
+          }`}
+          onClick={() => setActiveStep(index)} // Update activeStep on click
+        >
+          <div className="flex flex-col lg:flex-row">
+            <div className="lg:mr-2">{stepItem.icon}</div>
+            <div>{stepItem.name}</div>
+          </div>
+          {index !== activeStep && (
+            <div className="h-1 w-8 bg-gray-300 mx-2" /> // Add line between steps
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CheckOutSteps;
