29cdb66dd05e3998189270121b48bba752d1efc3
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-16 18:16:52 +0530

    create payment page using stripe

---
+++ client/src/components/cart/Payment.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentForm from "./PaymentForm";
+import { server } from "../../main";
+
+function Payment() {
+  const [stripeKey, setStripeKey] = useState("");
+
+  async function getStripeKey() {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+
+    try {
+      const { data } = await axios.get(
+        `${server}/payment/stripeapikey`,
+        config
+      );
+      setStripeKey(data.stripeApiKey);
+    } catch (error) {
+      console.error("Error fetching Stripe API key:", error);
+    }
+  }
+
+  useEffect(() => {
+    getStripeKey();
+  }, []);
+
+  return (
+    <>
+      {stripeKey && (
+        <Elements stripe={loadStripe(stripeKey)}>
+          <PaymentForm />
+        </Elements>
+      )}
+    </>
+  );
+}
+
+export default Payment;
