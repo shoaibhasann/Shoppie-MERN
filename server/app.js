078a6ecc106485929b0c7c768818bb3c22ec82fc
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-16 15:28:04 +0530

    implement stripe payment gateway

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
-import express from 'express';
-import productRoutes from './routes/product.route.js';
-import userRoutes from './routes/user.route.js';
-import orderRoutes from './routes/order.route.js';
+import * as dotenv from "dotenv";
+import express from "express";
+import productRoutes from "./routes/product.route.js";
+import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+// loding enviroment variables
+dotenv.config();
 
 // create an instance of express app
 const app = express();
 
-// Middleware for Parsing request body 
+// Middleware for Parsing request body
 app.use(express.json());
 
 // Middleware for url decode
@@ -18,23 +23,28 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware for parsing cookie
 app.use(cookieParser());
 
-app.use(cors({
-    origin: 'http://localhost:5173',
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     credentials: true,
-}));
+  })
+);
 
 // Handling product routes
-app.use('/api/v1/products', productRoutes);
+app.use("/api/v1/products", productRoutes);
 
 // Handling user routes
-app.use('/api/v1', userRoutes);
+app.use("/api/v1", userRoutes);
 
 // Handle order  routes
-app.use('/api/v1/orders', orderRoutes);
+app.use("/api/v1/orders", orderRoutes);
+
+// Handle payment routes
+app.use("/api/v1/payment", paymentRoutes);
 
 // Handling not defined routes
-app.use("*", (req,res) => {
-    res.status(404).send("Oops! page not found");
+app.use("*", (req, res) => {
+  res.status(404).send("Oops! page not found");
 });
 
 // Middleware for error handling
