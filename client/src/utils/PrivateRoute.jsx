86d88ff9cafb6edd1205357aeb00c0d0aa4a6e23
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-19 22:02:07 +0530

    implement protected route functionality

--- client/src/utils/PrivateRoute.jsx
+++ client/src/utils/PrivateRoute.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-function PrivateRoute({ component: Compotent, authenticated }) {
-  return authenticated ? <Compotent/> : <Navigate to='/login'/>
+function PrivateRoute({ component: Compotent, authenticated, loading }) {
+  if(!loading){
+    if(authenticated === true){
+      return <Compotent/>
+    } else{
+      return <Navigate to='/login'/>
+    }
+  }
 }
 
 export default PrivateRoute;
