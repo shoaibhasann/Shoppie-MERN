1b0ec5d43e2f6205303c75d0dfa36881c798e868
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-22 22:53:26 +0530

    create sidebar for admin panel

---
+++ client/src/components/admin/Sidebar.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  ShoppingBagIcon,
+  PowerIcon,
+  UserGroupIcon,
+  ChatBubbleOvalLeftIcon,
+  ReceiptRefundIcon,
+} from "@heroicons/react/24/solid";
+import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+ 
+function Sidebar() {
+
+  const [open, setOpen] = React.useState(0);
+ 
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+ 
+  return (
+    <Card className=" bg-slate-900 text-white w-full md:mx-0  md:max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <div className="mb-2 p-4">
+        <Typography variant="h5" color="blue-gray">
+          Admin Panel
+        </Typography>
+      </div>
+      <List>
+        <Accordion
+          open={open === 1}
+          icon={
+            <ChevronDownIcon
+              strokeWidth={2.5}
+              className={`mx-auto h-4 w-4 transition-transform ${
+                open === 1 ? "rotate-180" : ""
+              }`}
+            />
+          }
+        >
+          <ListItem className="p-0" selected={open === 1}>
+            <AccordionHeader
+              onClick={() => handleOpen(1)}
+              className="border-b-0 p-3"
+            >
+              <ListItemPrefix>
+                <PresentationChartBarIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              <Typography
+                color="blue-gray"
+                className="ml-2 mr-auto font-normal"
+              >
+                Dashboard
+              </Typography>
+            </AccordionHeader>
+          </ListItem>
+          <AccordionBody className="py-1 text-white">
+            <List className="p-0">
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                <Link to="/admin/dashboard">Analytics</Link>
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                Reporting
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                Projects
+              </ListItem>
+            </List>
+          </AccordionBody>
+        </Accordion>
+        <Accordion
+          open={open === 2}
+          icon={
+            <ChevronDownIcon
+              strokeWidth={2.5}
+              className={`mx-auto h-4 w-4 transition-transform ${
+                open === 2 ? "rotate-180" : ""
+              }`}
+            />
+          }
+        >
+          <ListItem className="p-0" selected={open === 2}>
+            <AccordionHeader
+              onClick={() => handleOpen(2)}
+              className="border-b-0 p-3"
+            >
+              <ListItemPrefix>
+                <ShoppingBagIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              <Typography
+                color="blue-gray"
+                className="ml-2 mr-auto font-normal"
+              >
+                Products
+              </Typography>
+            </AccordionHeader>
+          </ListItem>
+          <AccordionBody className="py-1 text-white">
+            <List className="p-0">
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                <Link to="/admin/products">All</Link>
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                </ListItemPrefix>
+                <Link to="/admin/product">Create</Link>
+              </ListItem>
+            </List>
+          </AccordionBody>
+        </Accordion>
+        <ListItem>
+          <ListItemPrefix>
+            <ReceiptRefundIcon className="h-5 w-5 mr-2" />
+          </ListItemPrefix>
+          <Link to="/admin/orders">Orders</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <UserGroupIcon className="h-5 w-5 mr-2" />
+          </ListItemPrefix>
+          <Link to="/admin/users">Users</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <ChatBubbleOvalLeftIcon className="h-5 w-5 mr-2" />
+          </ListItemPrefix>
+          <Link to="/admin/reviews">Reviews</Link>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5 mr-2" />
+          </ListItemPrefix>
+          Log Out
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
+
+export default Sidebar;
