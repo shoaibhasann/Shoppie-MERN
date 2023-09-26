06d7a41648543b73d6fa0ecbd9c9bb02f3628331
shoaibhasann <shoaibhasan0940@gmail.com>
2023-09-26 12:29:17 +0530

    completed product list component

--- client/src/components/admin/ProductListContent.jsx
+++ client/src/components/admin/ProductListContent.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect } from 'react';
 import { DataGrid } from "@mui/x-data-grid";
-import { PencilSquareIcon, TrashIcon} from "@heroicons/react/24/solid";
 import Sidebar from './SideBar';
 import MetaData from '../layout/MetaData';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchAllProudcts, productsReset } from '../../redux/AdminSlice';
+import { deleteProductReset, productsReset } from '../../redux/Admin/AdminSlice';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-tailwind/react';
 import '../../styles/ProductListContent.css';
 import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai'
+import Loader from '../layout/Loader';
+import { deleteProduct, fetchAllProudcts } from '../../redux/Admin/AdminAsyncActions';
 
 
 function ProductListContent() {
       const dispatch = useDispatch();
 
-      const { error, products } = useSelector((state) => state.admin);
+      const { error, products, productDeletion: { loading, isDeleted, error: deletionError} } = useSelector((state) => state.admin);
 
 
       useEffect(() => {
@@ -24,8 +25,21 @@ function ProductListContent() {
           dispatch(productsReset());
         }
 
+        if(deletionError){
+          toast.error(deletionError);
+        }
+
+        if(isDeleted){
+          toast.success('Product deleted successfuly!');
+          dispatch(deleteProductReset);
+        }
+
         dispatch(fetchAllProudcts());
-      }, [dispatch, error]);
+      }, [dispatch, error, toast, isDeleted, deletionError]);
+
+      const handleDelete = (id) => {
+        dispatch(deleteProduct(id));
+      };
 
  const columns = [
    { field: "id", headerName: "Product ID", minWidth: 200, flex: 0.5 },
@@ -67,7 +81,7 @@ function ProductListContent() {
                <AiOutlineEdit className="text-lg lg:text-xl font-medium" />
              </Link>
 
-             <Button className="p-1.5">
+             <Button onClick={() => handleDelete(params.id)} className="p-1.5">
                <AiOutlineDelete className="text-lg lg:text-xl font-medium" />
              </Button>
            </div>
@@ -93,23 +107,27 @@ function ProductListContent() {
    <>
      <MetaData title={`All Products - Admin`} />
 
-     <div className="max-w-[1240px] mt-8 mx-auto flex flex-col lg:flex-row">
-       <Sidebar />
-       <div className="bg-gray-100 lg:max-w-[calc(1240px-256px)]">
-         <div className='m-6'>
-          <h1 className="text-2xl font-semibold mb-4">ALL PRODUCTS</h1>
+     {loading ? (
+       <Loader />
+     ) : (
+       <div className="max-w-[1240px] mt-8 mx-auto flex flex-col lg:flex-row">
+         <Sidebar />
+         <div className="bg-gray-100 lg:max-w-[calc(1240px-256px)]">
+           <div className="m-6">
+             <h1 className="text-2xl font-semibold mb-4">ALL PRODUCTS</h1>
 
-         <DataGrid
-           rows={rows}
-           columns={columns}
-           pageSize={10}
-           disableSelectionOnClick
-           className="productListTable"
-           autoHeight
-         />
+             <DataGrid
+               rows={rows}
+               columns={columns}
+               pageSize={10}
+               disableSelectionOnClick
+               className="productListTable"
+               autoHeight
+             />
+           </div>
          </div>
        </div>
-     </div>
+     )}
    </>
  );
 }
